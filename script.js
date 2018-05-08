790cb673ba1968ca8fc8b3457d484c17d0808b62
rawatsonali96 <rawatsonali08@gmail.com>
2018-05-08 11:04:39 +0530

    changed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var mysql =require('mysql');
 var app=express();
+var bodyParser = require('body-parser');
 app.use(express.json());
 
 var connection=mysql.createConnection({
@@ -19,15 +20,45 @@ connection.connect(function(error){
 	}
 }); 
 
-app.post('/add',function(req,res){
-	connection.query("INSERT INTO sampleTable (id,name, email) VALUES (3, 'Mike','sty@gmail.com')", function(error,row,fields)
+var app = express();
+ 
+// create application/json parser
+// var jsonParser = bodyParser.json();
+ 
+// create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+// parse application/json
+app.use(bodyParser.json())
+ 
+// POST /login gets urlencoded bodies
+/*app.post('/login', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.send('welcome, ' + req.body.username)
+*/
+ 
+
+app.post('/add', function(req,res){
+
+	txtName = req.body.name
+	emailName = req.body.email
+	console.log(txtName, emailName, req.body, "print the query")
+	query = `INSERT INTO sampleTable (id,name, email) VALUES (3, ${txtName}, ${emailName})`
+    connection.query(query, function(error,row,fields)
      {
+
      	if(error){
-     console.log('error in insertion');
+     console.log('error in insertion', error);
      }else{
       console.log('data inserted',row[3]);
      }});
-});
+    });
+  
+	
 
 app.get('/',function(req,res){
 	 connection.query ("SELECT * from sampleTable", function(error,row,fields)
