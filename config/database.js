24e1531d227c2e806817577bb3e184555ea5f823
rawatsonali96 <rawatsonali08@gmail.com>
2018-06-21 23:11:56 +0530

    new commit

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'rawat@08'
-    },
-	'database': 'cloudprint',
-    'users_table': 'userinfo'
-};
